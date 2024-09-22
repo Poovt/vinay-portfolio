d4040228713d9900502e332d741c09113e2564ba
library-management <deglurep@gmail.com>
2024-09-22 17:39:26 +0530

    changes made

--- index.js
+++ index.js
@@ -17,7 +17,3 @@ menu.onscroll = ()=>{
     navlist.classList.remove('open')
 }
 
-let more = document.querySelectorAll('.more');
-for (let i=0; i<more.length; i++){
-  more[i].addEventListener('click',function(){ more[i].parentNode.classList.toggle('active')})
-}
